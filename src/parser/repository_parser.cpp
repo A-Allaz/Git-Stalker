e19e3e06a224619af7b92be105219a7fedcf6de7
Adam Allaz <adam.allaz@novainsilico.ai>
2026-07-21 11:50:39 +0200

    chore: repo parser implementation

--- src/parser/repository_parser.cpp
+++ src/parser/repository_parser.cpp
@@ -3,21 +3,37 @@
 
 #include <vector>
 #include <filesystem>
+#include <algorithm>
 #include "../../../include/datatypes/repository/repository.h"
 
 namespace fs = std::filesystem;
 
 std::vector<Repository*> parse_repositories(){
     std::vector<Repository*> repositories;
-    fs::path root_dir = "../" + fs::current_path().string();  // Gets the asssumed home directory
+    fs::path root_dir = "~" + fs::current_path().string();  // Gets the asssumed home directory
     
-    for(const auto &entry : fs::directory_iterator(root_dir)){
-        if(entry.path().string().find(".git") != std::string::npos){
-            if()
+    for(auto iter = fs::recursive_directory_iterator(root_dir); iter != fs::recursive_directory_iterator();)
+    {
+        if(iter->path().string().find(".git") != std::string::npos && iter->path().string().find(".gitignore") == std::string::npos)
+        {
+            std::string repo_name = iter->path().parent_path().string();
+            Repository* tmp = new Repository(repo_name, root_dir.string() + repo_name, nullptr);
+
+            if(std::find(repositories.begin(), repositories.end(), tmp) == repositories.end())
+            {
+                repositories.push_back(tmp);
+            } else {
+                delete tmp;
+            }
+
+            // go back to the previous folder and skip loop iteration (already managed by "iter.pop()")
+            iter.pop();
+            continue;
         }
+        iter++;
     }
 
-    return;
+    return repositories;
 };
 
 #endif
